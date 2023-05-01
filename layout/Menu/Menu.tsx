7f851c3424b9705f865964dd0407372ee78df83b
Dmitry <dmitryavgn@mail.ru>
2023-05-01 13:50:41 +0500

    o

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -93,8 +93,6 @@ export const Menu = (): JSX.Element => {
     };
 
     return (
-        <div className={styles.menu}>
-            {buildFirstLevel()}
-        </div>
+        <div className={styles.menu}>{buildFirstLevel()}</div>
     );
 };
